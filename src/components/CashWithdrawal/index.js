6daaf4ecc8521d086d17764c0bca7bafa3eda74a
RaviTejaKundrapu <ravi.kundrapu123@gmail.com>
2024-05-04 14:37:05 +0000

    10.2-Debugging-Cash-Withdrawal

---
+++ src/components/CashWithdrawal/index.js
@@ -0,0 +1,42 @@
+import {Component} from 'react'
+import DenominationItem from '../DenominationItem'
+import './index.css'
+
+class CashWithdrawal extends Component {
+  state = {
+    balance: 2000,
+  }
+
+  updateBalance = value => {
+    this.setState(prevState => ({balance: prevState.balance - value}))
+  }
+
+  render() {
+    const {denominationsList} = this.props
+    const {balance} = this.state
+
+    return (
+      <div className="app-container">
+        <div className="cash-withdrawal-container">
+          <div className="balance-container">
+            <p className="your-balance">Your Balance</p>
+            <p className="balance">{balance}</p>
+          </div>
+          <p className="withdraw">Withdraw</p>
+          <p className="choose-sum">CHOOSE SUM (IN RUPEES)</p>
+          <ul className="denominations-list">
+            {denominationsList.map(eachDenomination => (
+              <DenominationItem
+                key={eachDenomination.id}
+                denomination={eachDenomination}
+                updateBalance={this.updateBalance}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CashWithdrawal
---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,23 @@
+import './index.css'
+
+const DenominationItem = ({denomination, updateBalance}) => {
+  const {value} = denomination
+
+  const onClickDenomination = () => {
+    updateBalance(value)
+  }
+
+  return (
+    <li className="denomination-item">
+      <button
+        type="button"
+        className="denomination-button"
+        onClick={onClickDenomination}
+      >
+        {value}
+      </button>
+    </li>
+  )
+}
+
+export default DenominationItem
